84c18108e0d8ef609e40d5726fa0b04eb368156b
SankalpC10 <sgchavhan_b18@et.vjti.ac.in>
2023-06-04 01:31:29 +0530

    first commit

---
+++ src/main/kotlin/com/kotlinrest/kotlinrest/KotlinrestApplication.kt
@@ -0,0 +1,11 @@
+package com.kotlinrest.kotlinrest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinrestApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinrestApplication>(*args)
+}
